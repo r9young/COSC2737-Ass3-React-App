2bb63fb1e4add5c098d224ee5373d383bfe66a00
Yang <r9young@hotmail.com>
2024-06-07 20:53:41 +1000

    test2

--- src/App.js
+++ src/App.js
@@ -1,76 +1,3 @@
-
-// import axios from 'axios';
-// import { useState, useEffect } from 'react';
-// import DetailsCardComponent from "./components/DetailsCardComponent";
-
-// function App() {
-//   const [formData, setFormData] = useState({ name: "", email: "" });
-//   const [recordData, setRecordData] = useState([]);
-
-//   console.log("process.env:", process.env);
-//   console.log("process.env.REACT_APP_NODE_ENV:", process.env.REACT_APP_NODE_ENV);
-//   console.log("process.env.REACT_APP_SERVER_BASE_URL:", process.env.REACT_APP_SERVER_BASE_URL);
-//   const base_url = process.env.REACT_APP_NODE_ENV === 'development' ? process.env.REACT_APP_LOCAL_BASE_URL : process.env.REACT_APP_SERVER_BASE_URL;
-
-//   useEffect(() => {
-//     axios.get(`${base_url.replace(/\/$/, "")}/getUser`).then(res => {
-//       setRecordData(res.data);
-//     }).catch(err => alert(`Some error occurred ==> ${err}`));
-//   }, []);
-
-//   const handleChange = (event) => {
-//     const { name, value } = event.target;
-//     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-//   };
-
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-//     axios.post(`${base_url.replace(/\/$/, "")}/addUser`, formData).then(res => {
-//       setFormData({ name: "", email: "" });
-//       alert("User created successfully");
-//     }).catch(err => alert(`Some error occurred ==> ${err}`));
-//   };
-
-//   return (
-//     <div className="App">
-//       <nav className="navbar navbar-light bg-light mb-2">
-//         <a className="navbar-brand" href="https://www.youtube.com/@IntegrationNinjas">
-//           <img src="./logo_p.png" width="50" height="50" className="d-inline-block" alt="" />
-//           Easy Talk
-//         </a>
-//       </nav>
-//       <div className='container'>
-//         <div className="row">
-//           <div className="col">
-//             <h3 className="text-center">Users List</h3>
-//             <ul>
-//               {recordData.map((r, i) => <tl key={i}><DetailsCardComponent email={r.email} sn={i+1} userN={r.name} /></tl>)}
-//             </ul>
-//           </div>
-//           <div className="col">
-//             <h2>Add Users</h2>
-//             <form onSubmit={handleSubmit}>
-//               <div className="form-group">
-//                 <label htmlFor="exampleInputUser">User Name</label>
-//                 <input type="text" name="name" className="form-control" id="exampleInputUser" value={formData.name} onChange={handleChange} placeholder="Enter user name" />
-//               </div>
-//               <div className="form-group">
-//                 <label htmlFor="exampleInputEmail">Email</label>
-//                 <input type="email" name="email" className="form-control" id="exampleInputEmail" value={formData.email} onChange={handleChange} placeholder="Enter email" />
-//               </div>
-//               <button type="submit" className="btn btn-primary mt-2">Submit</button>
-//             </form>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 import DetailsCardComponent from "./components/DetailsCardComponent";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -84,13 +11,8 @@ function App() {
   console.log("process.env.REACT_APP_SERVER_BASE_URL:", process.env.REACT_APP_SERVER_BASE_URL);
   const base_url = process.env.REACT_APP_NODE_ENV === 'development' ? process.env.REACT_APP_LOCAL_BASE_URL : process.env.REACT_APP_SERVER_BASE_URL;
 
-//   useEffect(() => {
-//     axios.get(`${base_url}/getUser`).then(res => { setRecordData(res.data) }).catch(err => alert(`Some error occured ==>${err}`));
-//    }, []);
   useEffect(() => {
-        axios.get(`${base_url.replace(/\/$/, "")}/getUser`).then(res => {
-        setRecordData(res.data);
-        }).catch(err => alert(`Some error occurred ==> ${err}`));
+   axios.get(`${base_url}/getUsers`).then(res => { setRecordData(res.data) }).catch(err => alert(`Some error occured ==>${err}`));
   }, []);
 
   const handleChange = (event) => {
@@ -106,9 +28,9 @@ function App() {
   return (
     <div className="App">
       <nav className="navbar navbar-light bg-light mb-2">
-        <a className="navbar-brand" href="https://www.youtube.com/@IntegrationNinjas">
+        <a class="navbar-brand" href="https://www.youtube.com/@IntegrationNinjas">
           <img src="./logo_p.png" width="50" height="50" class="d-inline-block" alt="" />
-          Easy Talk
+          Integration Ninja
         </a>
       </nav>
       <div className='container'>
@@ -127,7 +49,7 @@ function App() {
                 <input type="text" name="name" className="form-control" id="exampleInputUser" value={formData.name} onChange={handleChange} placeholder="Enter user name" />
               </div>
               <div className="form-group">
-                <label htmlFor="exampleInputEmail">Email</label>
+                <label for="exampleInputEmail">Email</label>
                 <input type="email" name="email" className="form-control" id="exampleInputEmail" value={formData.email} onChange={handleChange} placeholder="Enter email" />
               </div>
               <button type="submit" className="btn btn-primary mt-2">Submit</button>

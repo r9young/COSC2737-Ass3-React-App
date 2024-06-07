841656c1b27f8db03690238c5c6ec3d7581047e5
Yang <r9young@hotmail.com>
2024-06-07 18:03:28 +1000

    getUser not getUsers

--- src/App.js
+++ src/App.js
@@ -11,12 +11,8 @@ function App() {
   console.log("process.env.REACT_APP_SERVER_BASE_URL:", process.env.REACT_APP_SERVER_BASE_URL);
   const base_url = process.env.REACT_APP_NODE_ENV === 'development' ? process.env.REACT_APP_LOCAL_BASE_URL : process.env.REACT_APP_SERVER_BASE_URL;
 
-  // useEffect(() => {
-  //  axios.get(`${base_url}/getUsers`).then(res => { setRecordData(res.data) }).catch(err => alert(`Some error occured ==>${err}`));
-  // }, []);
-
   useEffect(() => {
-    axios.get(`${base_url}/getUsers`).then(res => { setRecordData(res.data) }).catch(err => alert(`Some error occured ==>${err}`));
+    axios.get(`${base_url}/getUser`).then(res => { setRecordData(res.data) }).catch(err => alert(`Some error occured ==>${err}`));
    }, []);
 
   const handleChange = (event) => {

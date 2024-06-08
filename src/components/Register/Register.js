104e322fb716e6858f86dcd1281328374f7f6328
Yang <r9young@hotmail.com>
2024-06-08 18:27:21 +1000

    get register back

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,24 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Register.css'; // Import the CSS file
 
-function Register() {
+function App() {
   const [formData, setFormData] = useState({
-    firstname: '',
-    lastname: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
+    firstname: "",
+    lastname: "",
+    username: "",
+    password: "",
+    confirmPassword: ""
   });
   const [passwordMatch, setPasswordMatch] = useState(true);
+
+  console.log("process.env:", process.env);
+  console.log("process.env.REACT_APP_NODE_ENV:", process.env.REACT_APP_NODE_ENV);
+  console.log("process.env.REACT_APP_SERVER_BASE_URL:", process.env.REACT_APP_SERVER_BASE_URL);
+  
+  const base_url = process.env.REACT_APP_NODE_ENV === 'development'
+    ? process.env.REACT_APP_LOCAL_BASE_URL
+    : process.env.REACT_APP_SERVER_BASE_URL;
+
+  useEffect(() => {
+    axios.get(`${base_url.replace(/\/$/, "")}/getUser`).then(res => {
+      console.log(res.data);
+    }).catch(err => alert(`Some error occurred ==> ${err}`));
+  }, [base_url]);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
 
-    if (name === 'password' || name === 'confirmPassword') {
+    // Check password match
+    if (name === "password" || name === "confirmPassword") {
       setPasswordMatch(
-        name === 'password'
+        name === "password"
           ? value === formData.confirmPassword
           : value === formData.password
       );
@@ -32,80 +46,62 @@ function Register() {
       return;
     }
 
-    const { confirmPassword, ...dataToSubmit } = formData;
-    try {
-      await axios.post('/your-endpoint', dataToSubmit);
-      setFormData({
-        firstname: '',
-        lastname: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-      });
-      alert('User created successfully');
-    } catch (error) {
-      console.error('Error creating user:', error);
-      alert('Some error occurred');
-    }
+    const { confirmPassword, ...dataToSubmit } = formData; // Exclude confirmPassword
+    axios.post(`${base_url.replace(/\/$/, "")}/addUser`, dataToSubmit)
+      .then(res => {
+        setFormData({
+          firstname: "",
+          lastname: "",
+          username: "",
+          password: "",
+          confirmPassword: ""
+        });
+        alert("User created successfully");
+      })
+      .catch(err => alert(`Some error occurred ==> ${err}`));
   };
 
   return (
-    <div className="register">
-      <div className="content">
-        <h1 className="text-wrapper">Register Page</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="input-and-button">
-            <input
-              className="field"
-              type="text"
-              name="firstname"
-              value={formData.firstname}
-              onChange={handleChange}
-              placeholder="First Name"
-            />
-            <input
-              className="field"
-              type="text"
-              name="lastname"
-              value={formData.lastname}
-              onChange={handleChange}
-              placeholder="Last Name"
-            />
-            <input
-              className="field"
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Email"
-            />
-            <input
-              className="field"
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              placeholder="Password"
-            />
-            <input
-              className="field"
-              type="password"
-              name="confirmPassword"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-              placeholder="Confirm Password"
-            />
-            {!passwordMatch && (
-              <p className="error-message">Passwords do not match!</p>
-            )}
-            <button className="button" type="submit">
-              <span className="button-primary">Register</span>
-            </button>
+    <div className="App">
+      <nav className="navbar navbar-light bg-light mb-2">
+        <a className="navbar-brand" href="https://www.youtube.com/@IntegrationNinjas">
+          <img src="./logo_p.png" width="50" height="50" className="d-inline-block" alt="" />
+          Easy Talk
+        </a>
+      </nav>
+      <div className='container'>
+        <div className="row">
+          <div className="col">
+            <h2>New User Registration</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="exampleInputUser">First Name</label>
+                <input type="text" name="firstname" className="form-control" id="exampleInputUser" value={formData.firstname} onChange={handleChange} placeholder="Enter your first name" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputUser">Last Name</label>
+                <input type="text" name="lastname" className="form-control" id="exampleInputUser" value={formData.lastname} onChange={handleChange} placeholder="Enter your last name" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputUser">User Name</label>
+                <input type="email" name="username" className="form-control" id="exampleInputUser" value={formData.username} onChange={handleChange} placeholder="Enter a username" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputEmail">Enter Your Password</label>
+                <input type="password" name="password" className="form-control" id="exampleInputEmail" value={formData.password} onChange={handleChange} placeholder="Password" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputEmail">Re-enter Password</label>
+                <input type="password" name="confirmPassword" className="form-control" id="exampleInputEmail" value={formData.confirmPassword} onChange={handleChange} placeholder="Confirm Password" />
+              </div>
+              {!passwordMatch && <p style={{ color: 'red' }}>Passwords do not match!</p>}
+              <button type="submit" className="btn btn-primary mt-2">Submit</button>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
 }
 
-export default Register;
+export default App;
